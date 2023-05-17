46da69de00fc9fd6f4fcd81b833b18706d574a9c
raphael <kyeongseong.kim@kurlycorp.com>
2023-05-17 10:03:28 +0900

    fix: jmeter test시 css, js error를 해결한다.

---
+++ src/main/java/org/springframework/samples/petclinic/system/WebConfiguration.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.petclinic.system;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("/webjars/")
+            .resourceChain(false);
+        registry.setOrder(1);
+    }
+
+}
